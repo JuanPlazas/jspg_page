18e77242021698d220d5b14cf90e00c19d3b95d8
JuanPlazas <juan-2151@hotmail.com>
2026-07-30 13:08:49 -0500

    feat #08: se agrega componente Contact, se estandariza estructura de carpetas

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -61,7 +61,7 @@ export function ProjectCard({ project, glow }: ProjectCardProps) {
 							<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
 							<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
 						</svg>
-						ver repositorio
+						Link
 					</a>
 					<span className="text-text-muted/50 mt-3">{flipIcon}</span>
 				</div>
